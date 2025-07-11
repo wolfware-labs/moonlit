cf1e653d671a2d2238661a641cef1bd7d271dd0d
Mariano Santoro <mariano.santoro@wolfware.dev>
2025-07-10 20:38:43 -0500

    refactor(logging): remove unused regex and simplify `ConsoleLoggerProvider`
    - Eliminated `CategoryRegex` and related methods as they were unused.
    - Removed partial modifier from `ConsoleLoggerProvider`, simplifying its implementation.

--- Wolfware.Moonlit/Wolfware.Moonlit.Cli/src/Logging/ConsoleLoggerProvider.cs
+++ Wolfware.Moonlit/Wolfware.Moonlit.Cli/src/Logging/ConsoleLoggerProvider.cs
@@ -1,11 +1,9 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Wolfware.Moonlit.Cli.Logging;
 
-public partial class ConsoleLoggerProvider : ILoggerProvider
+public class ConsoleLoggerProvider : ILoggerProvider
 {
   private readonly IDisposable? _onChange;
   private LoggerFilterOptions _filterOptions;
@@ -28,36 +26,4 @@ public partial class ConsoleLoggerProvider : ILoggerProvider
   {
     _onChange?.Dispose();
   }
-
-  private string GetCategory(string categoryName)
-  {
-    if (ConsoleLoggerProvider.CategoryRegex().Match(categoryName) is not {Success: true} match)
-    {
-      return categoryName;
-    }
-
-    string sdkName = match.Groups["context"].Value;
-    string className = match.Groups["className"].Value;
-
-    var categoryBuilder = new StringBuilder();
-    if (!string.IsNullOrWhiteSpace(sdkName))
-    {
-      categoryBuilder.Append(sdkName);
-      categoryBuilder.Append('.');
-    }
-
-    if (!string.IsNullOrWhiteSpace(className))
-    {
-      categoryBuilder.Append(className);
-    }
-    else if (categoryBuilder.Length > 0)
-    {
-      categoryBuilder.Length--;
-    }
-
-    return categoryBuilder.ToString();
-  }
-
-  [GeneratedRegex(@"^.+\.(?<className>.+)$")]
-  private static partial Regex CategoryRegex();
 }
