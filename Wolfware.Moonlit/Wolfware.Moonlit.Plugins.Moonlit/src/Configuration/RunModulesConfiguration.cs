360939e5ddaded13c7458750b8084d59af553bd6
Mariano Santoro <mariano.santoro@wolfware.dev>
2025-07-16 23:02:47 -0500

    feat(moonlit-plugin): initial implementation of RunModules plugin

---
+++ Wolfware.Moonlit/Wolfware.Moonlit.Plugins.Moonlit/src/Configuration/RunModulesConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace Wolfware.Moonlit.Plugins.Moonlit.Configuration;
+
+public sealed class RunModulesConfiguration
+{
+  public required string[] ModulePaths { get; set; } = [];
+}
---
+++ Wolfware.Moonlit/Wolfware.Moonlit.Plugins.Moonlit/src/Middlewares/RunModules.cs
@@ -0,0 +1,12 @@
+﻿using Wolfware.Moonlit.Plugins.Moonlit.Configuration;
+using Wolfware.Moonlit.Plugins.Pipelines;
+
+namespace Wolfware.Moonlit.Plugins.Moonlit.Middlewares;
+
+public sealed class RunModules : ReleaseMiddleware<RunModulesConfiguration>
+{
+  protected override Task<MiddlewareResult> ExecuteAsync(ReleaseContext context, RunModulesConfiguration configuration)
+  {
+    throw new NotImplementedException();
+  }
+}
