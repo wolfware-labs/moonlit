b4a6d791c3bed0a023e04a66aa00b06c647bfac8
Mariano Santoro <mariano.santoro@wolfware.dev>
2025-07-23 21:37:29 -0500

    feat(plugin-semantic-release): add logging to `ConvertCommits` middleware
    - Introduced `ILogger<ConvertCommits>` for improved logging support.
    - Added logs to track commit conversion and filtering process.
    - Updated constructor to inject `ILogger` dependency.

--- Wolfware.Moonlit/Wolfware.Moonlit.Plugins.SemanticRelease/src/Middlewares/ConvertCommits.cs
+++ Wolfware.Moonlit/Wolfware.Moonlit.Plugins.SemanticRelease/src/Middlewares/ConvertCommits.cs
@@ -1,4 +1,5 @@
-﻿using Wolfware.Moonlit.Plugins.Pipelines;
+﻿using Microsoft.Extensions.Logging;
+using Wolfware.Moonlit.Plugins.Pipelines;
 using Wolfware.Moonlit.Plugins.SemanticRelease.Configuration;
 using Wolfware.Moonlit.Plugins.SemanticRelease.Models;
 using Wolfware.Moonlit.Plugins.SemanticRelease.Services;
@@ -7,19 +8,23 @@ namespace Wolfware.Moonlit.Plugins.SemanticRelease.Middlewares;
 
 public sealed class ConvertCommits : ReleaseMiddleware<ConvertCommitsConfiguration>
 {
+  private readonly ILogger<ConvertCommits> _logger;
   private readonly SharedContext _sharedContext;
 
-  public ConvertCommits(SharedContext sharedContext)
+  public ConvertCommits(ILogger<ConvertCommits> logger, SharedContext sharedContext)
   {
+    _logger = logger;
     _sharedContext = sharedContext;
   }
 
   protected override Task<MiddlewareResult> ExecuteAsync(ReleaseContext context,
     ConvertCommitsConfiguration configuration)
   {
+    this._logger.LogInformation("Converting {Count} commits", configuration.Commits.Length);
     var commits = ConventionalCommitConverter.Convert(configuration.Commits);
     commits = commits.Where(c => ConvertCommits.CheckCommit(c, configuration)).ToArray();
     this._sharedContext.Commits = commits;
+    this._logger.LogInformation("Converted to {Count} conventional commits", commits.Length);
     return Task.FromResult(MiddlewareResult.Success(output =>
     {
       output.Add("commits", commits);
