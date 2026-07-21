33767014b0c42b7c8f533c2a465fbf0c70bfc8e7
Mariano Santoro <mariano.santoro@wolfware.dev>
2026-07-21 09:20:25 -0500

    feat(git): add commits middleware with boundary resolution

--- plugins/git/src/lib.rs
+++ plugins/git/src/lib.rs
@@ -1,12 +1,14 @@
 //! Moonlit first-party `git` plugin. Shells to the `git` CLI via the host
 //! process capability; one component instance per pipeline run holds `GitShared`.
 
+mod commits;
 mod latest_tag;
 mod repo_context;
 mod shared;
 
 use moonlit_plugin_sdk::prelude::*;
 
+use commits::Commits;
 use latest_tag::LatestTag;
 use repo_context::RepoContext;
 use shared::GitShared;
@@ -14,5 +16,5 @@ use shared::GitShared;
 moonlit_plugin! {
     name: "git",
     state: GitShared,
-    middlewares: [RepoContext, LatestTag],
+    middlewares: [RepoContext, LatestTag, Commits],
 }
