167ff1d4fd9b9836faf6b496b38f2e77e3575196
Mariano Santoro <mariano.santoro@wolfware.dev>
2026-07-22 14:45:38 -0500

    feat(dotnet): test middleware and plugin wiring

--- plugins/dotnet/src/lib.rs
+++ plugins/dotnet/src/lib.rs
@@ -6,5 +6,20 @@ mod config;
 mod dotnet;
 mod pack;
 mod push;
+mod test;
 mod trx;
 mod version;
+
+use moonlit_plugin_sdk::prelude::*;
+
+use build::Build;
+use config::DotnetConfig;
+use pack::Pack;
+use push::Push;
+use test::Test;
+
+moonlit_plugin! {
+    name: "dotnet",
+    config: DotnetConfig,
+    middlewares: [Build, Pack, Push, Test],
+}
