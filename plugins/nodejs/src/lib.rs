c81e44ecd5180304e216d66f48309317c8cee594
Mariano Santoro <mariano.santoro@wolfware.dev>
2026-07-23 10:20:45 -0500

    feat(nodejs): build middleware

--- plugins/nodejs/src/lib.rs
+++ plugins/nodejs/src/lib.rs
@@ -1,6 +1,7 @@
 //! Moonlit first-party `nodejs` plugin. Shells out to the `npm` CLI for
 //! install / run-script / build / pack / push / test. One component instance per run.
 
+mod build;
 mod config;
 mod install;
 mod npm;
